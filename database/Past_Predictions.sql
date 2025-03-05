cf962dad3842a6aaa537a6816f5052e6c98a76cb
SodaChan <chansodara.dy@epita.fr>
2025-03-05 12:43:33 +0100

    Updated queries for creating Data & Past Prediction Tables. Added a Setup Guide

--- database/Past_Predictions.sql
+++ database/Past_Predictions.sql
@@ -1,29 +1,31 @@
--- Table: public.past_predictions
+-- SET search_path TO dsp;
 
--- DROP TABLE IF EXISTS public.past_predictions;
+-- Table: dsp.past_predictions
 
-CREATE TABLE past_predictions (
+-- DROP TABLE IF EXISTS dsp.past_predictions;
+
+CREATE TABLE dsp.past_predictions (
     id SERIAL PRIMARY KEY,      
     features JSON NOT NULL,        
     insertion_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,  
     prediction integer            
 );
 
-SELECT * FROM past_predictions ORDER BY insertion_timestamp DESC;
+-- SELECT * FROM dsp.past_predictions ORDER BY insertion_timestamp DESC;
 
 
 -- SELECT features ->>'xxxxx' AS xxxxx,      
--- FROM past_predictions;
+-- FROM dsp.past_predictions;
 
 
--- DELETE FROM past_predictions
+-- DELETE FROM dsp.past_predictions
 -- WHERE features->>'xxx_id' = 'ID No.'; -- Deletes a specific entry
 
--- DELETE FROM past_predictions
--- WHERE id = (SELECT MAX(id) FROM past_predictions); -- Deletes most recent
+-- DELETE FROM dsp.past_predictions
+-- WHERE id = (SELECT MAX(id) FROM dsp.past_predictions); -- Deletes most recent
 
--- ALTER SEQUENCE past_predictions_id_seq RESTART WITH 1; -- Resets auto-increment ID to 1
+-- ALTER SEQUENCE dsp.past_predictions_id_seq RESTART WITH 1; -- Resets auto-increment ID to 1
 
 
--- DELETE FROM past_predictions; -- Fully clears the table
--- TRUNCATE TABLE past_predictions RESTART IDENTITY; -- RESET EVERYTHING BUTTON
+-- DELETE FROM dsp.past_predictions; -- Fully clears the table
+-- TRUNCATE TABLE dsp.past_predictions RESTART IDENTITY; -- RESET EVERYTHING BUTTON
