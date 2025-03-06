605be4ecb895d54e9c1801ed10cb4e0cf3853dec
Liam Vargas <vargasbecerra.liamemanuel7@gmail.com>
2025-03-06 10:11:09 +0100

    Created prediction DAG, README file; updated reqs, and .gitignore; overall improvements
    Added the following files:
    
    - prediction_dag.py
    - README.md
    
    Updated requirements.txt:
    
    - duckdb
    
    Overall improvements:
    
    - In ingestion_dag.py deleted imported classes not in use
    - Changed dag id of ingestion-dag to ingestion_dag
    - Added max_active_runs=1 in ingestion-dag
    - Use of duckdb instead of pandas to enhance flexibility
    - In .gitignore included data/good_data/prediction_checklist.txt
    - Improved code readability

--- airflow/dags/ingestion_dag.py
+++ airflow/dags/ingestion_dag.py
@@ -1,23 +1,20 @@
 from pendulum import datetime
-from airflow import DAG, Dataset
+from airflow import DAG
 from airflow.decorators import task
-
-# ToDo:
-# Change the schedule
-# Change the start_date
 
 
 with DAG (
-    dag_id = 'ingestion-dag',
-    schedule = '@daily',
-    start_date = datetime(2025, 1, 1),
-    catchup = False
+    dag_id='ingestion_dag',
+    schedule='*/1 * * * *',
+    start_date=datetime(2025, 1, 1),
+    catchup=False,
+    max_active_runs=1
 ) as dag:
 
     @task.bash
     def read_data(ti):
         from pathlib import Path
-        import pandas as pd
+        import duckdb
         import subprocess
         
         # Declare the raw_data folder path
@@ -25,16 +22,22 @@ with DAG (
 
         # Check if there are available csv files to ingest
         if list(p.glob('*.csv')):
+
             # Pick a random csv file from the raw_data directory
-            random_file = subprocess.getoutput(f'bash $AIRFLOW_HOME/scripts/shell/random_file.sh ')
-            # Read its contents
-            current_data = pd.read_csv(random_file)
+            random_file_path = subprocess.getoutput(f'bash $AIRFLOW_HOME/scripts/shell/random_file.sh ')
+
+            # Read its contents with duckdb
+            conn = duckdb.connect(':memory:')
+            current_file = conn.execute(f'SELECT * FROM read_csv("{random_file_path}");')
+
             # Push the Dataframe via XCOM
-            ti.xcom_push(key='new_raw_data', value=current_data)
+            ti.xcom_push(key='new_raw_data', value=current_file.fetchdf())
+
             # Return the path of the random csv via XCOM
-            return f'echo "{random_file}"'
+            return f'echo "{random_file_path}"'
+        
         else:
-            # If there are no files, mark the task execution state as skipped
+            # If there are no files, skip
             return 'exit 99;'
     
     _read_data = read_data()
@@ -42,12 +45,10 @@ with DAG (
 
     @task.bash
     def save_file(ti):
-        # df = ti.xcom_pull(key='new_raw_data') Not in use for the moment
         # Read the XCOM with key "return_value" pushed by read_data task and move file to good data folder
-        return 'mv {{ ti.xcom_pull(key="return_value") }} $AIRFLOW_HOME/data/good_data'
+        return 'mv {{ ti.xcom_pull(key="return_value") }} $AIRFLOW_HOME/data/good_data && echo {{ ti.xcom_pull(key="return_value") }}'
 
     _save_file = save_file()
-
 
 # Execution order of tasks
 _read_data >> _save_file
