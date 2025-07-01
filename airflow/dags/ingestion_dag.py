cfc34d97e61b982929c338c9639264af8d36e9ad
Liam Vargas <vargasbecerra.liamemanuel7@gmail.com>
2025-07-01 16:07:33 +0200

    - Improved split_dataset script: 	- Now it accepts number of partitios 	- 'gender' features keeps its category "NA" intact 	- Dataset can be shuffled before partitioning 	- The split_dataset function can be used thought the terminal
    - Mocking issues can be set to True and False via the Airflow Variables
    
    - Mock issues implemented specific functions to mock issues on target features
    
    - Implemented GXE operator with data validation
    
    - Added airflow-provider-great-expectations==1.0.0a3 to requirements
    
    - Added SQL scripts to create schema and tables for ingestion statistics

--- airflow/dags/ingestion_dag.py
+++ airflow/dags/ingestion_dag.py
@@ -1,6 +1,96 @@
-from pendulum import datetime
+from pendulum import datetime # type: ignore
+
 from airflow import DAG
-from airflow.decorators import task
+from airflow.decorators import task # type:ignore
+from airflow.models import Variable # type:ignore
+from airflow.operators.dummy import DummyOperator # type:ignore 
+
+from modules.mock_data_issues.generate_data_issues import generate_data_issues #type: ignore
+
+from great_expectations import ExpectationSuite
+import great_expectations.expectations as gxe # type: ignore
+from great_expectations_provider.operators.validate_dataframe import ( # type: ignore
+    GXValidateDataFrameOperator)
+
+
+def retrieve_df_for_gx_validation():
+    import duckdb # type: ignore
+    
+    conn = duckdb.connect('ingest.ddb')
+    df = conn.execute('SELECT * FROM latest').fetchdf()
+    return df
+
+
+expectation_suite = ExpectationSuite(
+    name='marketing_data_validation',
+    expectations = [
+        # 1. Number of features expected
+        gxe.ExpectTableColumnCountToEqual(value=40),
+        # 2. New features and # 3. Missing featuress
+        gxe.ExpectTableColumnsToMatchSet(column_set=[
+            'gradyear', 'gender', 'age', 
+            'NumberOffriends', 'basketball','football', 
+            'soccer', 'softball', 'volleyball', 
+            'swimming','cheerleading', 'baseball', 
+            'tennis', 'sports', 'cute', 
+            'sex', 'sexy','hot', 'kissed', 
+            'dance', 'band', 'marching', 
+            'music', 'rock', 'god',
+            'church', 'jesus', 'bible', 
+            'hair', 'dress', 'blonde', 
+            'mall','shopping', 'clothes', 
+             'hollister', 'abercrombie', 'die', 
+            'death','drunk', 'drugs']
+            ),
+        # 4. New Categories
+        gxe.ExpectColumnValuesToBeInSet(
+            column='gender', value_set=['F', 'M', 'NA']
+        ),
+        gxe.ExpectColumnValuesToBeBetween(
+            column='gradyear', min_value=2006, max_value=2009
+        ),
+        gxe.ExpectColumnMaxToBeBetween(
+            column='NumberOffriends', min_value=0, max_value=1000
+        ),
+        # 5. Null Values
+        gxe.ExpectColumnValuesToNotBeNull(
+            column='gradyear'
+        ),
+        gxe.ExpectColumnValuesToNotBeNull(
+            column='gender'
+        ),
+        gxe.ExpectColumnValuesToNotBeNull(
+            column='NumberOffriends'
+        ),
+        gxe.ExpectColumnValuesToNotBeNull(
+            column='age'
+        ),
+        gxe.ExpectColumnValuesToNotBeNull(
+            column='drunk'
+        ),
+        gxe.ExpectColumnValuesToNotBeNull(
+            column='drugs'
+        ),
+        gxe.ExpectColumnValuesToNotBeNull(
+            column='volleyball'
+        ),
+        # 6. Error Type
+        gxe.ExpectColumnValuesToBeOfType(
+            column='volleyball', type_='int16'
+        ),
+        gxe.ExpectColumnValuesToBeOfType(
+            column='drugs', type_='int16'
+        ),
+        gxe.ExpectColumnValuesToBeOfType(
+            column='drunk', type_='int16'
+        ),
+        # 7. Wrong input
+        gxe.ExpectColumnValuesToMatchRegex(
+            column="age",
+            regex=r"^\d+(\.\d+)?$"
+        )
+    ]
+)
 
 
 with DAG (
@@ -10,13 +100,16 @@ with DAG (
     catchup=False,
     max_active_runs=1
 ) as dag:
-
+    
     @task.bash
     def read_data(ti):
         from pathlib import Path
-        import duckdb
+        import duckdb # type: ignore
         import subprocess
-        
+
+        mock_issues = True if Variable.get('mock_issues') == 'True' else False
+
+        print(f"Are we mocking issues today? {'yes' if mock_issues else 'no'}")
         # Declare the raw_data folder path
         p = Path('/opt/airflow/data/raw_data')
 
@@ -27,12 +120,19 @@ with DAG (
             random_file_path = subprocess.getoutput(f'bash $AIRFLOW_HOME/scripts/shell/random_file.sh ')
 
             # Read its contents with duckdb
-            conn = duckdb.connect(':memory:')
-            current_file = conn.execute(f'SELECT * FROM read_csv("{random_file_path}");')
+            conn = duckdb.connect('ingest.ddb')
+            conn.execute(f"CREATE OR REPLACE TABLE latest AS SELECT * FROM read_csv_auto('{random_file_path}', nullstr='');")
+            df = conn.execute('SELECT * FROM latest').fetchdf()
 
-            # Push the Dataframe via XCOM
-            ti.xcom_push(key='new_raw_data', value=current_file.fetchdf())
+            if mock_issues:
+                print("Generating data issues...")
+                df = generate_data_issues(dataframe=df, chances=0.5)
+                conn.execute('CREATE OR REPLACE TABLE latest AS SELECT * FROM df')
 
+            df = conn.execute('SELECT * FROM latest').fetchdf()
+            print(f'Read DF columns: {df.columns}')
+            print(f'Read DF num cols: {len(df.columns)}')
+            
             # Return the path of the random csv via XCOM
             return f'echo "{random_file_path}"'
         
@@ -42,13 +142,122 @@ with DAG (
     
     _read_data = read_data()
 
+    
+    validate_data = GXValidateDataFrameOperator(
+        task_id="validate_data",
+        configure_dataframe=retrieve_df_for_gx_validation,
+        expect=expectation_suite,
+        context_type="ephemeral",
+        result_format="COMPLETE",
+    )
+
 
     @task.bash
     def save_file(ti):
-        # Read the XCOM with key "return_value" pushed by read_data task and move file to good data folder
-        return 'mv {{ ti.xcom_pull(key="return_value") }} $AIRFLOW_HOME/data/good_data && echo {{ ti.xcom_pull(key="return_value") }}'
+        import duckdb
+        
+        conn = duckdb.connect('ingest.ddb')
+        df = conn.execute('SELECT * FROM latest').fetchdf()
+        print(f'DF columns: {df.columns}')
+        print(f'Number of columns: {len(df.columns)}')
+        current_file_name = ti.xcom_pull(task_ids="read_data", key="return_value").split('/')[5]
+        bad_data_save_path = f'/opt/airflow/data/bad_data/bad_{current_file_name}'
+        good_data_save_path = f'/opt/airflow/data/good_data/{current_file_name}'
+        
+        gx_output = ti.xcom_pull(task_ids='validate_data', key='return_value')
+        successful_validation = gx_output['success']
+        successful_feature_validation = False
 
+        print(f'Successful validation? {successful_validation}')
+        print(f'Expected features? {successful_feature_validation}')
+
+        
+        for expectation in gx_output['expectations']:
+            if (expectation['expectation_type'] == 'expect_table_columns_to_match_set' and expectation['success'] == True):
+                successful_feature_validation = True
+                break
+
+        if successful_validation and successful_feature_validation:
+            conn.execute(
+                    f"""
+                    COPY (
+                        SELECT *
+                        FROM latest
+                    ) TO '{good_data_save_path}' (HEADER, DELIMITER ',');
+                    """
+                )
+
+        
+        # If columns set matches:
+        if not successful_validation and successful_feature_validation:          
+            bad_data_idx = []
+            for expectation in gx_output['expectations']:
+                if expectation['success'] == False:
+                    column = expectation['kwargs'].get('column')
+                    idx_list = expectation['result'].get('unexpected_index_list')
+                    if column and idx_list:
+                        bad_data_idx.extend(expectation['result']['unexpected_index_list'])
+            
+            if bad_data_idx:
+                bad_data_idx = ', '.join(str(idx) for idx in {*bad_data_idx})    
+            # Retrieving the bad rows
+                conn.execute(
+                    f"""
+                    COPY (
+                        WITH bad_data AS (
+                            SELECT *, row_number() OVER () - 1 AS rn
+                            FROM latest
+                        )
+                        SELECT * EXCLUDE rn
+                        FROM bad_data
+                        WHERE rn IN ({bad_data_idx})
+                    ) TO '{bad_data_save_path}' (HEADER, DELIMITER ',');
+                    """
+                )
+            
+            # Retrieving the good rows
+                conn.execute(
+                    f"""
+                    COPY (
+                        WITH good_data AS (
+                            SELECT *, row_number() OVER () - 1 AS rn
+                            FROM latest
+                        )
+                        SELECT * EXCLUDE rn
+                        FROM good_data
+                        WHERE rn NOT IN ({bad_data_idx})
+                    ) TO '{good_data_save_path}' (HEADER, DELIMITER ',');
+                    """
+                )
+            else:
+                conn.execute(
+                    f"""
+                    COPY (
+                        SELECT *
+                        FROM latest
+                    ) TO '{good_data_save_path}' (HEADER, DELIMITER ',');
+                    """
+                )
+
+        # If column set does not matches
+        elif not successful_validation and not successful_feature_validation:
+            conn.execute(
+                    f"""
+                    COPY (
+                        SELECT *
+                        FROM latest
+                    ) TO '{bad_data_save_path}' (HEADER, DELIMITER ',');
+                    """
+                )
+        # Read the XCOM with key "return_value" pushed by read_data task and move file to good data folder
+        return 'rm {{ ti.xcom_pull(task_ids="read_data", key="return_value") }} && echo {{ ti.xcom_pull(task_ids="read_data", key="return_value") }}'
+    
     _save_file = save_file()
 
+    dummy_task = DummyOperator(task_id='send_alerts')
+
+    dummy_task2 = DummyOperator(task_id='save_statistics')
+
+
 # Execution order of tasks
-_read_data >> _save_file
+_read_data >> validate_data >> [_save_file, dummy_task, dummy_task2]
--- mock_data_issues/generate_data_issues.py
+++ mock_data_issues/generate_data_issues.py
@@ -4,14 +4,15 @@ from typing import Callable
 import random
 
 
-def coin_flip(chanches: float = 0.80) -> bool:
-    if random.random() < chanches:
+def coin_flip(chances: float = 0.80) -> bool:
+    if random.random() < chances:
         return True
     else:
         return False
 
 
 def produce_outliers(value: str | int | None) -> str | int:
+    print(f'Producing outlier, input value={value}')
     if str(value).isalpha():
         return value
     elif value is None:
@@ -23,6 +24,7 @@ def produce_outliers(value: str | int | None) -> str | int:
 
 
 def set_string(value: str | int | None) -> str:
+    print(f'Setting as String, input value={value}')
     if str(value).isalpha():
         return value
     elif str(value).isnumeric():
@@ -30,54 +32,153 @@ def set_string(value: str | int | None) -> str:
 
 
 def set_numeric(value: str | int | None) -> str | int:
+    print(f'Setting numerical value, input value={value}')
     if str(value).isalpha():
         return value
     elif value is None:
         return pd.NA
     elif isinstance(value, str):
-        return np.random.randint(0, 2009)
+        return int(np.random.randint(0, 2009))
 
 
-def new_categories(value: str) -> str:
-    if value in ['F', 'M']:
-        return np.random.choice(['B', 'NB', 'FTM', 'MTF', 'OTHER', ''])
- 
-
-def produce_nulls(value: str | int | None) ->   None:
+def produce_nulls(value: str | int | None) -> pd.NA:
+    print('Producing Null value')
     value = pd.NA
     return value
 
 
-def apply_funcs(dataframe: pd.DataFrame, funcs: list[Callable], row: int) -> pd.DataFrame:
-    random_affected_cols = np.random.choice(np.arange(0, 39), size=len(funcs), replace=False)
-    for func, col  in zip(funcs, random_affected_cols):
-        current_value = dataframe.iat[row, col]
-        data_issue = func(current_value) 
-        dataframe.iloc[[row], [col]] = data_issue
+def new_categories(value: str) -> str:
+    if random.random() > 0.1:
+        if value in ['F', 'M', 'NA']:
+            print(f'Producing new category for gender, input value={value}')
+            return np.random.choice(['B', 'NB', 'FTM', 'MTF', 'OTHER', ''])
+        else:
+            return produce_nulls(value)
+
+
+def grad_year_issues(value: int) -> int | None:
+    lower_grad_bound = np.random.randint(1900, 2005)
+    upper_grad_bound = np.random.randint(2010, 2025)
+    
+    if random.random() > 0.3:
+        print('Producing grad out of bounds values')
+        return np.random.choice([lower_grad_bound, upper_grad_bound])
+    else:
+        if random.random() > 0.5:
+            print('Producing null')
+            return produce_nulls(value)
+        else:
+            print('Producing outliers')
+            return produce_outliers(value)
+
+
+def age_data_issues(value: float) -> int | None:
+    random_old_age = np.random.randint(100, 150)
+    if random.random() > 0.3:
+        print('Producing unrealistic ages')
+        return random_old_age
+    else:
+        if random.random() > 0.5:
+            print('Producing null')
+            return produce_nulls(value)
+        else:
+            print('Producing outliers')
+            return produce_outliers(value)
+
+
+def number_of_friends_issues(value: int) -> int | None:
+    rand_num = np.random.randint(606, 10_000)
+
+    if random.random() > 0.5:
+        print('Producing more num of friends')
+        return rand_num
+    else:
+        print('Producing null')
+        return produce_nulls(value)
+
+
+def new_feature(dataframe: pd.DataFrame) -> pd.DataFrame:
+    protected_cols = ['gradyear', 'gender', 'age', 'NumberOffriends', 'volleyball', 'drunk', 'drugs']
+    rename_candidates = [col for col in dataframe.columns if col not in protected_cols]
+
+    new_features = ['iphone', 'highschool', 'summer', 'videogames', 'youtube', 'lol', 'tacos', 'tequila']
+    random_new_feature = np.random.choice(new_features)
+
+    if random.random() > 0.5:
+        if rename_candidates:
+            current_col_name = np.random.choice(rename_candidates)
+            if not random_new_feature in dataframe.columns:
+                dataframe = dataframe.rename(columns={current_col_name: random_new_feature})
+            return dataframe
+    else:
+        dataframe[random_new_feature] = np.random.choice([0, 1], size=len(dataframe))
+
+    return dataframe
+
+
+issues_dict = {
+    1: produce_nulls,
+    2: new_categories,
+    3: set_numeric,
+    4: set_string,
+    5: produce_outliers,
+}
+
+
+def apply_funcs(dataframe: pd.DataFrame, row: int, funcs: list[Callable]) -> pd.DataFrame:
+    random_col = np.random.choice([
+        'gradyear', 'gender', 'age', 
+        'NumberOffriends', 'volleyball', 'drunk', 'drugs'
+    ])
+    
+    col_idx = dataframe.columns.get_loc(random_col)
+    current_value = dataframe.iat[row, col_idx]
+
+    if random.random() > 0.3:
+        if random_col == 'gradyear':
+            data_issue = grad_year_issues(current_value)
+        elif random_col == 'gender':
+            data_issue = new_categories(current_value)
+        elif random_col == 'age':
+            data_issue = age_data_issues(current_value)
+        elif random_col == 'NumberOffriends':
+            data_issue = number_of_friends_issues(current_value)
+        elif random_col in ['volleyball', 'drunk', 'drugs']:
+            rand_func = np.random.choice(funcs)
+            data_issue = rand_func(current_value)
+        
+        dataframe.iat[row, col_idx] = data_issue
+        print(f"Affected column = {random_col}, affected row = {row}")
+    else:
+        print('Producing new feature')
+        dataframe = new_feature(dataframe)
+
     return dataframe
 
 
 def set_data_issues(dataframe: pd.DataFrame, affected_rows: dict) -> pd.DataFrame:
-    check_key_value_match = [k for (k,v) in affected_rows.items() if k == v]
-    issues_dict = {1: produce_nulls, 2: new_categories, 3: set_numeric, 4: set_string, 5: produce_outliers}
-    if check_key_value_match:
-        col_number = np.random.randint(0, 39)
-        return dataframe.drop(dataframe.columns[col_number], axis=1)
+    check_key_value_match = [k for (k, v) in affected_rows.items() if k == v]
+    print(f'Dropping col? {'yes' if check_key_value_match else 'no'}')
+    protected_cols = ['gradyear', 'gender', 'age', 'NumberOffriends', 'volleyball', 'drunk', 'drugs']
+    unprotected_columns = [col for col in dataframe.columns if col not in protected_cols]
 
+    if unprotected_columns and random.random() < 0.2:
+        col_to_drop = np.random.choice(unprotected_columns)
+        print(f"Dropped column: {col_to_drop}")
+        return dataframe.drop(columns=[col_to_drop])
     else:
         for k, v in affected_rows.items():
             pick_random_issue = np.random.choice(np.arange(1, 6), size=5, replace=False)
             funcs = [func for (k, func) in issues_dict.items() if k in pick_random_issue]
-            dataframe = apply_funcs(dataframe, funcs, row=k)
-            
+            dataframe = apply_funcs(dataframe, funcs=funcs, row=k)
         return dataframe
 
 
 def determine_affected_rows(dataframe: pd.DataFrame) -> dict:
     number_of_rows = dataframe.shape[0] - 1
+    print(f'Dataframe shape {dataframe.shape}')
     afected_rows = np.random.randint(1, number_of_rows, size=number_of_rows)
     set_issues_dict = {}
-
     for idx in afected_rows:
         if idx not in set_issues_dict.keys():
             set_issues_dict[idx] = 1
@@ -90,6 +191,8 @@ def generate_data_issues(dataframe: pd.DataFrame = None, chances: float = None) 
     if coin_flip(chances):
         affected_rows = determine_affected_rows(dataframe)
         dataframe = set_data_issues(dataframe, affected_rows)
+        for col in ['drunk', 'volleyball', 'drugs']:
+            dataframe[col] = pd.to_numeric(dataframe[col], errors='coerce').astype('Int16')
         return dataframe
     else:
         return dataframe
